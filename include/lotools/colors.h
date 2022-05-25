c964f7a08afc0cc2660497619d1a6da75ffe80c0
lomekragow <shieldpotocol@outlook.com>
2022-05-25 20:53:20 +0800

    Add color switch for colors.h

--- include/lotools/colors.h
+++ include/lotools/colors.h
@@ -10,24 +10,88 @@ constexpr const char* begin_red = "\033[31m";
 constexpr const char* begin_blue = "\033[1;34m";
 constexpr const char* color_reset = "\033[0m";
 
-inline std::string green(std::string_view raw_text)
+class colors
 {
-    return begin_green + std::string(raw_text) + color_reset;
-}
+public:
+    [[nodiscard]] static bool get_color_switch()
+    {
+        return color_switch();
+    }
 
-inline std::string yellow(std::string_view raw_text)
-{
-    return begin_yellow + std::string(raw_text) + color_reset;
-}
+    static void set_color_switch(bool is_open)
+    {
+        color_switch() = is_open;
+    }
 
-inline std::string red(std::string_view raw_text)
-{
-    return begin_red + std::string(raw_text) + color_reset;
-}
+    static std::string green(std::string_view raw_text)
+    {
+        if (color_switch())
+            return begin_green + std::string(raw_text) + color_reset;
+        return std::string(raw_text);
+    }
 
-inline std::string blue(std::string_view raw_text)
-{
-    return begin_blue + std::string(raw_text) + color_reset;
-}
+    static std::string yellow(std::string_view raw_text)
+    {
+        if (color_switch())
+            return begin_yellow + std::string(raw_text) + color_reset;
+        return std::string(raw_text);
+    }
+
+    static std::string red(std::string_view raw_text)
+    {
+        if (color_switch())
+            return begin_red + std::string(raw_text) + color_reset;
+        return std::string(raw_text);
+    }
+
+    static std::string blue(std::string_view raw_text)
+    {
+        if (color_switch())
+            return begin_blue + std::string(raw_text) + color_reset;
+        return std::string(raw_text);
+    }
+
+    static std::string begin_greenf()
+    {
+        if (color_switch())
+            return begin_green;
+        return {};
+    }
+
+    static std::string begin_yellowf()
+    {
+        if (color_switch())
+            return begin_yellow;
+        return {};
+    }
+
+    static std::string begin_redf()
+    {
+        if (color_switch())
+            return begin_red;
+        return {};
+    }
+
+    static std::string begin_bluef()
+    {
+        if (color_switch())
+            return begin_blue;
+        return {};
+    }
+
+    static std::string color_resetf()
+    {
+        if (color_switch())
+            return color_reset;
+        return {};
+    }
+
+private:
+    static bool& color_switch()
+    {
+        static bool color_switch_ = true;
+        return color_switch_;
+    }
+};
 
 } // namespace lot
